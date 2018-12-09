cb2b1c7027fbb82120d99318cf27738446ec8748
Ryan Parkerson <rparkerson@users.noreply.github.com>
2018-12-08 19:30:48 -0500

    updated comments in excercise5_3.rb

--- 5flow_control/excercise5_3.rb
+++ 5flow_control/excercise5_3.rb
@@ -2,6 +2,7 @@
 # and reports back whether the number is between 0 and 50,
 # 51 and 100, or above 100.
 
+# Exclusive endpoints for the word between
 puts "Pick a number between 0 and 100"
 number = gets.chomp.to_i
   if number > 0 && number < 50
@@ -12,7 +13,8 @@ number = gets.chomp.to_i
     puts "Your number is above 100"
   end
 
-# Alternative:
+# Alternative: inclusive endpoints for the word "between" from
+# example solution
 
 puts "Please enter a number between 0 and 100."
 number = gets.chomp.to_i
